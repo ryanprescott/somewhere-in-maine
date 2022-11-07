6994ef6acf57e9334e044e42231781c1783b878c
Ryan Prescott <rppgoat@gmail.com>
2022-11-07 11:04:25 -0500

    Ripple effect is working nicely

--- src/lib/components/Button.svelte
+++ src/lib/components/Button.svelte
@@ -1,14 +1,50 @@
 <script lang="ts">
     export let width: number | string | null | undefined = 'auto';
     export let height: number | string | null | undefined = 'auto';
-    export let onClick: (() => void) | undefined;
 
-    let isRippling = false;
-    let timeout: any;
+    import { createEventDispatcher } from 'svelte';
+
+    const dispatch = createEventDispatcher();
+
+    let rippleTop = 0, rippleLeft = 0;
+
+    let isRippled = false;
+
+    const ripple = (e: MouseEvent) => {
+        rippleTop = e.offsetY;
+        rippleLeft = e.offsetX;
+
+        isRippled = false;
+        setTimeout(() => isRippled = true, 10);
+
+    };
+
+    const mousedown = (e: MouseEvent) => {
+        ripple(e);
+        dispatch('mousedown', e);
+    };
+
+    const blur = (e: FocusEvent) => {
+        isRippled = false;
+        dispatch('blur', e);
+    };
+    
 </script>
 
 <style lang="scss">
     @import '../../theme/theme.scss';
+
+    @keyframes ripple {
+        0% {
+            transform: translate(-50%, -50%) scale(0);
+            opacity: 1;
+        }
+        100% {
+            transform: translate(-50%, -50%) scale(1);
+            opacity: 0.2;
+        }
+    }
+
     .button {
         &-ripple {
             &-container {
@@ -18,20 +54,28 @@
             }
             &-effect {
                 position: absolute;
-                top: 50%;
-                left: 50%;
-                transform: translate(-50%, -50%) scale(0);
-                width: 120%;
-                padding-bottom: 120%;
+                transform: translate(-50%, -50%) scale(1);
+                transition: opacity 0.5s;
+                opacity: 0;
+                width: 300%;
+                padding-bottom: 300%;
                 background-color: $accent1;
-                transition: transform 0.2s;
                 border-radius: 100%;
+                box-shadow: $accent1 0 0 30px 8px;
+                &.rippled {
+                    animation: ripple 0.5s;
+                    transform: translate(-50%, -50%) scale(1);
+                    opacity: 0.2;
+                }
             }
         }
-        position: relative;
+
         display: flex;
+
         background-color: $background;
         color: $onSurface;
+
+        position: relative;
 
         transition: background-color 0.25s;
         border-radius: 12px;
@@ -42,21 +86,25 @@
             padding: 12px;
             z-index: 1;
         }
+
         &:hover {
             background-color: $accent2;
-        }
-        &:active {
-            background-color: inherit;
-            .button-ripple-effect {
-                transform: translate(-50%, -50%) scale(1);
-            }
         }
     }
 </style>
 
-<button class="button" width={width} height={height}>
+<button
+    class="button"
+    width={width}
+    height={height}
+    on:mousedown={mousedown}
+    on:blur={blur}
+>
     <div class='button-ripple-container'>
-        <span class='button-ripple-effect'></span>
+        <span 
+            class={`button-ripple-effect ${isRippled ? 'rippled' : ''}`}
+            style={`top: ${rippleTop}px; left: ${rippleLeft}px;`}
+        />
     </div>
     <span class='button-label'>
         <slot></slot>
