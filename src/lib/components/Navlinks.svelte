4e2a9290375b470b36e3977aae260275bb9d69d1
Ryan Prescott <rppgoat@gmail.com>
2022-11-08 08:43:33 -0500

    Add active class to Navlinks

--- src/lib/components/Navlinks.svelte
+++ src/lib/components/Navlinks.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+    import { page } from '$app/stores';
     const links = [
         {name: 'Home', path: '/'},
         {name: 'About', path: '/about'},
@@ -25,12 +26,20 @@
             padding: 6px 12px 6px 6px;
             margin: 0;
             list-style: none;
+            letter-spacing: 0.325px;
+            &.active {
+                color: $onSurface;
+                font-weight: bold;
+                letter-spacing: 0;
+            }
         }
     }
 </style>
 
 <ul class="navlinks" role="navigation">
     {#each links as link}
-        <a href={link.path}><li>{link.name}</li></a>
+        <a href={link.path}>
+            <li class:active={$page.url.pathname === link.path}>{link.name}</li>
+        </a>
     {/each}
 </ul>
